85e50b00fc81c331c0af4326c87df8ccabbdd1aa
Luismoteando <LuisMiguel.Ortiz1@alu.uclm.es>
2018-05-21 17:31:45 +0200

    Some changes after merging with Lydia

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_104831) do
+ActiveRecord::Schema.define(version: 2018_05_17_110640) do
 
   create_table "collections", force: :cascade do |t|
     t.string "title"
